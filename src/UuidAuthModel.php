65c1e6c936a4a868b1a65f6e33a4008849ab9142
Theodore R. Smith <theodore@phpexperts.pro>
2019-07-03 16:24:25 -0500

    Added the UuidAuthModel.

---
+++ src/UuidAuthModel.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace PHPExperts\ConciseUuid;
+
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Auth\MustVerifyEmail;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Foundation\Auth\Access\Authorizable;
+
+abstract class UuidAuthModel extends ConciseUuid implements
+    AuthenticatableContract,
+    AuthorizableContract,
+    CanResetPasswordContract
+{
+    use Authenticatable, Authorizable, CanResetPassword, MustVerifyEmail;
+}
